9b09e1f5c638dd9138586bc143287c2e81533207
RehanyTamas <109366607+RehanyTamas@users.noreply.github.com>
2023-07-13 21:59:06 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,11 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+  required_version = ">=0.14.9"
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
+  version = "~>3.0"
 }
 
 module "vpc" {
