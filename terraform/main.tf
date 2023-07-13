d4d99c404780ff3744ae15a3c952097df892fcba
RehanyTamas <109366607+RehanyTamas@users.noreply.github.com>
2023-07-13 22:15:38 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,12 @@
+
 terraform {
+  required_version = "~> 0.12.31"
+
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "> 4.0"
-    }
+    aws  = "~> 3.74.1"
   }
 }
+
 
 # Configure the AWS Provider
 provider "aws" {
