da149ec3801b948ac0ae03541c534796c47391b9
RehanyTamas <109366607+RehanyTamas@users.noreply.github.com>
2023-07-13 22:32:22 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-
-
 resource "aws_vpc" "main" {
  cidr_block = "10.0.0.0/16"
  tags = {
@@ -51,56 +49,11 @@ resource "aws_subnet" "private_subnets" {
 }
 
 variable "azs" {
-
  type        = list(string)
-
  description = "Availability Zones"
-
  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-
 }
 
-resource "aws_subnet" "public_subnets" {
-
- count             = length(var.public_subnet_cidrs)
-
- vpc_id            = aws_vpc.main.id
-
- cidr_block        = element(var.public_subnet_cidrs, count.index)
-
- availability_zone = element(var.azs, count.index)
-
- 
-
- tags = {
-
-   Name = "Public Subnet ${count.index + 1}"
-
- }
-
-}
-
- 
-
-resource "aws_subnet" "private_subnets" {
-
- count             = length(var.private_subnet_cidrs)
-
- vpc_id            = aws_vpc.main.id
-
- cidr_block        = element(var.private_subnet_cidrs, count.index)
-
- availability_zone = element(var.azs, count.index)
-
- 
-
- tags = {
-
-   Name = "Private Subnet ${count.index + 1}"
-
- }
-
-}
 
 resource "aws_internet_gateway" "gw" {
 
